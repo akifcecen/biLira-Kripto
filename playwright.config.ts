d91648cba706e8aa2c20585cda575ae23266d583
CCN <akifcecenn@yandex.com>
2025-03-07 22:26:07 +0300

    Update playwright.config.ts Headless mode activated

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     actionTimeout: 30000,
     baseURL: 'http://trendyol.com/',
     trace: 'on',
